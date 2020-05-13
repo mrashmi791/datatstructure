35ce3f9c526a83962eb66177c239d1ac99eed11e
Rashmi Mishra <mrashmi791@gmail.com>
2020-05-13 14:09:08 +0530

    Prefix to infix..

---
+++ src/com/rashmi/datastructure/practice/stack/PrefixToInfix.java
@@ -0,0 +1,53 @@
+package com.rashmi.datastructure.practice.stack;
+
+import java.util.Scanner;
+
+class SData {
+	SData next ;
+	String data ;
+	SData(String data) {
+		this.data = data;
+		;
+	}
+}
+public class PrefixToInfix {
+	 
+	static SData top = null;
+	static SData top1 = null;
+	
+	/*
+	 * create stack and push the data...
+	 */
+	public static void push(String d) {
+		SData con = new SData(d);
+//		if(top == null) {
+//			top = con;
+//		} else {
+			con.next = top;
+			top = con;
+//		}
+		
+	}
+	
+	/*
+	 * print stack data...
+	 */
+	public static void traverse() {
+		SData temp = top;
+		while (temp != null) {
+			System.out.print(temp.data +" ");
+			temp = temp.next;
+		}
+		
+	}
+	public static void main(String[] args) {
+		push("5");
+		push("+");
+		push("4");
+		push("*");
+		push("3");
+		traverse();
+		
+	}
+
+}
